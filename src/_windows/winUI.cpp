8a33b3a5a7964013acc2384195bbbe27834fff8d
Lucas K <70863326+Hedge239@users.noreply.github.com>
2024-01-03 00:31:23 -0500

    Added menu options

--- src/_windows/winUI.cpp
+++ src/_windows/winUI.cpp
@@ -16,31 +16,41 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
             // File
             HMENU hFileMenu = CreateMenu();
-                //AppendMenuW(hFileMenu, MF_STRING, 1, L"Open");
-                //AppendMenuW(hFileMenu, MF_STRING, 2, L"Exit");
-            AppendMenuW(hMenu, MF_POPUP, (UINT_PTR)hFileMenu, L"File");
+                AppendMenuW(hFileMenu, MF_STRING, 1, L"Open File");
+                AppendMenuW(hFileMenu, MF_STRING, 2, L"Open Folder");
+                AppendMenuW(hFileMenu, MF_STRING, 3, L"Open Project");
+                AppendMenuW(hFileMenu, MF_SEPARATOR, 0, nullptr);
+                AppendMenuW(hFileMenu, MF_STRING, 4, L"Save File");
+                AppendMenuW(hFileMenu, MF_STRING, 5, L"Save All");
+                AppendMenuW(hFileMenu, MF_SEPARATOR, 0, nullptr);
+                AppendMenuW(hFileMenu, MF_STRING, 6, L"Exit");
+                AppendMenuW(hMenu, MF_POPUP, (UINT_PTR)hFileMenu, L"File");
             // Edit
             HMENU hEditMenu = CreateMenu();
-            AppendMenuW(hMenu, MF_POPUP, (UINT_PTR)hEditMenu, L"Edit");
+                AppendMenuW(hMenu, MF_POPUP, (UINT_PTR)hEditMenu, L"Edit");
+                AppendMenuW(hEditMenu, MF_STRING, 1, L"Undo");
+                AppendMenuW(hEditMenu, MF_STRING, 2, L"Redo");
+                AppendMenuW(hEditMenu, MF_SEPARATOR, 0, nullptr);
+                AppendMenuW(hEditMenu, MF_STRING, 3, L"Find Text");
+                AppendMenuW(hEditMenu, MF_STRING, 4, L"Replace Text");
             // View
             HMENU hViewMenu = CreateMenu();
-            AppendMenuW(hMenu, MF_POPUP, (UINT_PTR)hViewMenu, L"View");
+                AppendMenuW(hMenu, MF_POPUP, (UINT_PTR)hViewMenu, L"View");
             // Project
             HMENU hProjectMenu = CreateMenu();
-            AppendMenuW(hMenu, MF_POPUP, (UINT_PTR)hProjectMenu, L"Project");
+                AppendMenuW(hMenu, MF_POPUP, (UINT_PTR)hProjectMenu, L"Project");
             // Extensions
             HMENU hPluginMenu = CreateMenu();
-            AppendMenuW(hMenu, MF_POPUP, (UINT_PTR)hPluginMenu, L"Plugin");
+                AppendMenuW(hMenu, MF_POPUP, (UINT_PTR)hPluginMenu, L"Plugin");
             // Settings
             HMENU hSettingsMenu = CreateMenu();
-            AppendMenuW(hMenu, MF_POPUP, (UINT_PTR)hSettingsMenu, L"Settings");
+                AppendMenuW(hMenu, MF_POPUP, (UINT_PTR)hSettingsMenu, L"Settings");
             // Help
             HMENU hHelpMenu = CreateMenu();
-            AppendMenuW(hMenu, MF_POPUP, (UINT_PTR)hHelpMenu, L"Help");
+                AppendMenuW(hMenu, MF_POPUP, (UINT_PTR)hHelpMenu, L"Help");
 
 
             // SIDEBAR //
-            // TODO
 
             SetMenu(hwnd, hMenu);
             break;
