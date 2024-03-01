6580b0d025f6f1742e2790727a8d79c83ca86bf2
Lucas K <70863326+Hedge239@users.noreply.github.com>
2024-03-01 15:15:19 -0500

    >:(

--- src/_windows/winUI.cpp
+++ src/_windows/winUI.cpp
@@ -82,7 +82,7 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPa
             AppendMenuW(g_hEditorMenu, MF_POPUP, (UINT_PTR)hHelpMenu, app::win32::system::StringToWideString(app::common::Localisation::GetText("menu_help")).c_str());
 
             // Textbox
-            g_hEditorTextBox =  CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("Edit"), "", WS_CHILD | WS_VISIBLE | ES_AUTOHSCROLL | ES_AUTOVSCROLL | ES_MULTILINE | WS_VSCROLL| WS_HSCROLL| WS_BORDER, 0, 0, 0, 0, hwnd, (HMENU)1, (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE), NULL);
+            g_hEditorTextBox =  CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("Edit"), "", WS_CHILD | WS_VISIBLE | ES_AUTOHSCROLL | ES_AUTOVSCROLL | ES_MULTILINE | WS_VSCROLL| WS_HSCROLL| WS_BORDER, 0, 0, 0, 0, hwnd, (HMENU)100, GetModuleHandle(NULL), NULL);
 
             // FileExploerer
 
@@ -95,10 +95,10 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPa
         {
             if(g_hEditorTextBox != NULL)
             {
-                //InvalidateRect(g_hEditorTextBox, NULL, TRUE);
+                InvalidateRect(g_hEditorTextBox, NULL, TRUE);
                 //UpdateWindow(g_hEditorTextBox);
                 MoveWindow(g_hEditorTextBox, 0, 0, LOWORD(lParam), HIWORD(lParam), TRUE);
-                InvalidateRect(g_hEditorTextBox, NULL, FALSE);
+                //InvalidateRect(g_hEditorTextBox, NULL, FALSE);
             }
 
             break;
